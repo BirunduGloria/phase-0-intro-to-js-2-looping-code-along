85881c138647beedaa0d15571adda4c273841b3b
Gloria Birundu <birundugloria@gmail.com>
2025-06-29 20:24:26 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// Function 1: writeCards
+function writeCards(names, eventName) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messages;
+}
+
+// Function 2: countDown
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
